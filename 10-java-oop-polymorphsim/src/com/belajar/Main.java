602a88cc85c0f9ecb329776bfe0c52af1bbc05bb
PAPA <85855470+pradanaasih1233@users.noreply.github.com>
2023-01-21 06:29:00 +0700

    belajar polimor

---
+++ 10-java-oop-polymorphsim/src/com/belajar/Main.java
@@ -0,0 +1,41 @@
+// PLYMORPHISM
+
+// Polymorphism berarti banyak bentuk, atau kemampuan sebuah objek untuk berubah bentuk menjadi bentuk lain.
+// Memiliki keterkaitan dengan Inheritance.
+
+/*------------------------------------------------------------
+ *| PLYMORPHISM
+ *|------------------------------------------------------------
+ *|  Polymorphism berarti banyak bentuk,
+ *| atau kemampuan sebuah objek untuk berubah bentuk.
+ */
+
+package com.belajar;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("BELAJAR JAVA OOP 09 object class");
+
+        // contoh :
+        // ini adalah cara merubah bentuk.
+        Kakek kakek = new Kakek("saya jarat");
+        siapaSaya(kakek);
+
+        // objek kakek berubah.
+        kakek = new OrangTua();
+        siapaSaya(kakek);
+
+        // objek kakek brubah.
+        kakek = new Orang("jarat","kulonprogo",true);
+        siapaSaya(kakek);
+
+
+    }
+
+    public static void siapaSaya(Kakek kakek){
+        System.out.println("---------------------------");
+        kakek.sayaAdalah();
+        System.out.println("---------------------------");
+    }
+
+}
