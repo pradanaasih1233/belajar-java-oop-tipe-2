8edb761f3240706bb4da2339d65c4cfd5f5ed83b
PAPA <85855470+pradanaasih1233@users.noreply.github.com>
2023-01-24 07:31:33 +0700

    Add files via upload

---
+++ 27-java-oop-record-class/src/com/belajar/Main.java
@@ -0,0 +1,29 @@
+// RECORD CLASS
+
+/*------------------------------------------------------------
+*| RECORD CLASS
+*|------------------------------------------------------------
+*|  Merupakan bagian dari class data yang akan bisa digunakan
+*| untuk menapung data dan megolah data.
+*|------------------------------------------------------------
+*| CIRI RECORD CLASS :
+*|------------------------------------------------------------
+*| - Class yang hanya berisikan data. Hanya berisi getter,equal,
+*|   hashcode dan tostring method.
+*| - Digunakan untuk mempermudah pembuatan jenis class tersebut.
+*| - Dalam record class otomatis ada getter, equal, hashcode, dan
+*|   tostring serta construktor.
+*| - Record class secara otomatis extends dari class java.lang.Record
+*|   yang membuat tidak dapat mengextend class lain. Tapi dapat
+*|   implement interface.
+*| - Tidak ada setter dalam record class.
+*/
+
+package com.belajar;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\nBELAJAR JAVA OOP 27 record class");
+
+    }
+}
